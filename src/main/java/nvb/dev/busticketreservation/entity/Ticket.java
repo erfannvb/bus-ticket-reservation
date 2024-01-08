0af4374d2e8841c62638c60928b4817171d1cca8
Erfan Navab <erfannavab@gmail.com>
2024-01-08 10:55:23 +0330

    - added User and Ticket entity - set their relationship

---
+++ src/main/java/nvb/dev/busticketreservation/entity/Ticket.java
@@ -0,0 +1,68 @@
+package nvb.dev.busticketreservation.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nvb.dev.busticketreservation.base.entity.BaseEntity;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tbl_ticket")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Ticket extends BaseEntity<Long> {
+
+    @NotNull(message = "ticketOwner cannot be null")
+    @NotEmpty(message = "ticketOwner cannot be empty")
+    @Column(name = "ticket_owner", nullable = false)
+    private String ticketOwner;
+    @NotNull(message = "start cannot be null")
+    @NotEmpty(message = "start cannot be empty")
+    @Column(name = "start", nullable = false)
+    private String start;
+
+    @NotNull(message = "destination cannot be null")
+    @NotEmpty(message = "destination cannot be empty")
+    @Column(name = "destination", nullable = false)
+    private String destination;
+
+    @NotNull(message = "moveDate cannot be null")
+    @NotEmpty(message = "moveDate cannot be empty")
+    @Column(name = "move_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private LocalDate moveDate;
+
+    @NotNull(message = "moveTime cannot be null")
+    @NotEmpty(message = "moveTime cannot be empty")
+    @Column(name = "move_time", nullable = false)
+    @Temporal(TemporalType.TIME)
+    private LocalDate moveTime;
+
+    @NotNull(message = "travelId cannot be null")
+    @NotEmpty(message = "travelId cannot be empty")
+    @Column(name = "travel_id", nullable = false)
+    private UUID travelId;
+
+    @ManyToMany(mappedBy = "ticketSet")
+    private Set<User> userSet = new HashSet<>();
+
+    public Ticket(String ticketOwner, String start, String destination,
+                  LocalDate moveDate, LocalDate moveTime, UUID travelId) {
+        this.ticketOwner = ticketOwner;
+        this.start = start;
+        this.destination = destination;
+        this.moveDate = moveDate;
+        this.moveTime = moveTime;
+        this.travelId = travelId;
+    }
+}
---
+++ src/main/java/nvb/dev/busticketreservation/entity/User.java
@@ -0,0 +1,62 @@
+package nvb.dev.busticketreservation.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nvb.dev.busticketreservation.base.entity.BaseEntity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tbl_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User extends BaseEntity<Long> {
+
+    @NotNull(message = "firstName cannot be null")
+    @NotEmpty(message = "firstName cannot be empty")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @NotNull(message = "lastName cannot be null")
+    @NotEmpty(message = "lastName cannot be empty")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NotNull(message = "username cannot be null")
+    @NotEmpty(message = "username cannot be empty")
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @NotNull(message = "password cannot be null")
+    @NotEmpty(message = "password cannot be empty")
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @NotNull(message = "firstName cannot be null")
+    @NotEmpty(message = "firstName cannot be empty")
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(name = "user_ticket",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "ticket_id", referencedColumnName = "id"))
+    private Set<Ticket> ticketSet = new HashSet<>();
+
+    public User(String firstName, String lastName, String username, String password, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.gender = gender;
+    }
+}
